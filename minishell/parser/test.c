e9bd4624e9c35ee151d5c5ba2261272b61538d75
Eduardo Corona <eduardo.corona040@gmail.com>
2024-06-24 22:46:28 +0100

    Add parser

---
+++ minishell/parser/test.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 15:43:48 by ecorona-          #+#    #+#             */
+/*   Updated: 2024/06/24 21:57:48 by ecorona-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+#include "readline/readline.h"
+
+int	ft_isexit(char *str)
+{
+	if (str)
+		if (*str++ == 'e')
+			if (*str++ == 'x')
+				if (*str++ == 'i')
+					if (*str++ == 't')
+						if (*str == '\0')
+							return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	char	*line;
+	t_token	tok;
+
+	line = NULL;
+	while (1)
+	{
+		line = readline("( ๑ ˃̵ᴗ˂̵)و ");
+		if (ft_isexit(line))
+		{
+			free(line);
+			break ;
+		}
+		tok = lex(line);
+		print_tree(parse(line));
+		free(line);
+	}
+	line = NULL;
+	tok = (t_token){0, NULL};
+}
