e5d637a2f7faa47e2dc39c0ed2012d6fa39d9ee2
Mariana Pereira <up202205093@g.uporto.pt>
2024-06-28 16:04:11 +0100

    finishing export

--- minishell/exec/exec_utils3.c
+++ minishell/exec/exec_utils3.c
@@ -6,7 +6,7 @@
 /*   By: mfaria-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:14:11 by mfaria-p          #+#    #+#             */
-/*   Updated: 2024/06/27 15:53:39 by mfaria-p         ###   ########.fr       */
+/*   Updated: 2024/06/28 16:01:34 by mfaria-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,39 @@ int	ft_isalpha(int c)
 	return (1024 * ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)));
 }
 
-//ft_strchr
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*str;
 
-//ft_strcpy
+	str = (char *)s;
+	i = 0;
+	while (s[i] != (unsigned char)c)
+	{
+		if (s[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return (&str[i]);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dest;
+
+	dest = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	ft_strlcpy(dest, s, ft_strlen(s) + 1);
+	return (dest);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
