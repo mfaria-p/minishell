5702251ae92df74f52ac6099775da8c52f2f99d7
Mariana Pereira <up202205093@g.uporto.pt>
2024-07-03 00:55:41 +0100

    falta acabar cd

--- minishell/exec/built_ins3.c
+++ minishell/exec/built_ins3.c
@@ -6,7 +6,7 @@
 /*   By: mfaria-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:18:12 by mfaria-p          #+#    #+#             */
-/*   Updated: 2024/06/30 21:52:42 by mfaria-p         ###   ########.fr       */
+/*   Updated: 2024/07/03 00:35:32 by mfaria-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,8 @@ int	is_valid_identifier(char *var, char *value)
 	int		i;
 	int		j;
 	char	*temp;
+	int		len;
+	int		i;
 
 	i = 0;
 	j = 0;
@@ -76,18 +78,111 @@ int	is_valid_identifier(char *var, char *value)
 // Function to find the variable in the environment
 int	find_var(char **envp, const char *var)
 {
-	int	len;
+	int	len1;
 	int	i;
+	char	*equal_sign;
+	int	len2;
 
-	len = ft_strlen(var);
+	len2 = 0;
+	equal_sign = ft_strchr(var, '=');
+	if (equal_sign != NULL)
+	{
+		*equal_sign = '\0';
+		len2 = ft_strlen(var);
+		*equal_sign = '=';
+	}
+	else
+		len2 = ft_strlen(var);
+	len1 = 0;
 	i = 0;
 	while (envp[i] != NULL)
 	{
-		if (ft_strncmp(envp[i], var, len) == 0 && (envp[i][len] == '='))
+		equal_sign = ft_strchr(envp[i], '=');
+		if (equal_sign != NULL)
+		{
+			*equal_sign = '\0';
+			len1 = ft_strlen(envp[i]);
+			*equal_sign = '=';
+		}
+		else
+			len1 = ft_strlen(envp[i]);
+		if (ft_strncmp(envp[i], var, len1) == 0 && (len1 == len2))
 		{
 			return (i);
 		}
 		i++;
 	}
 	return (-1);
+}
+
+char	*find_oldpwd(char **envp)
+{
+	while (*envp && ft_strncmp("OLDPWD=", *envp, 7) != 0)
+		envp++;
+	if (*envp)
+		return (*envp + 7);
+	return (NULL);
+}
+
+char	*create_env_var(const char *var, const char *value)
+{
+	char	*env_var;
+	size_t	var_len;
+	size_t	value_len;
+
+	var_len = strlen(var);
+	value_len = strlen(value);
+	env_var = malloc(var_len + value_len + 2); // +2 for '=' and null terminator
+	if (!env_var)
+		return (NULL);
+	strcpy(env_var, var);
+	strcat(env_var, "=");
+	strcat(env_var, value);
+	return (env_var);
+}
+
+void	ft_cd(char ***envp, char *path)
+{
+	char	*current;
+	char	*old_dir;
+	char	*oldpwd_var;
+	char	*pwd_var;
+
+	old_dir = find_oldpwd(*envp);
+	current = find_pwd(*envp);
+	if (ft_strlen(path) == 1 && path[0] == '-')
+	{
+		printf("%s\n", old_dir);
+		chdir(old_dir);
+	}
+	else
+		chdir(path);
+	if (current)
+	{
+		oldpwd_var = create_env_var("OLDPWD", current);
+		set_env_with_equal(envp, oldpwd_var);
+	}
+	current = find_pwd(*envp);
+	if (current)
+	{
+		pwd_var = create_env_var("PWD", current);
+		set_env_with_equal(envp, pwd_var);
+	}
+}
+
+char	*find_home(char **envp)
+{
+	while (*envp && ft_strncmp("HOME=", *envp, 5) != 0)
+		envp++;
+	if (*envp)
+		return (*envp + 5);
+	return (NULL);
+}
+
+void	ft_cd_home(char **envp)
+{
+	char	*home;
+
+	home = find_home(envp);
+	chdir(home);
 }
