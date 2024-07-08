02c30e6dd138e5820508d56e1c05f2e48e16bd69
Eduardo Corona <eduardo.corona040@gmail.com>
2024-07-08 20:33:37 +0100

    Fix fork

--- minishell/exec/execution2.c
+++ minishell/exec/execution2.c
@@ -6,7 +6,7 @@
 /*   By: mfaria-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 22:05:32 by mfaria-p          #+#    #+#             */
-/*   Updated: 2024/06/30 19:28:16 by mfaria-p         ###   ########.fr       */
+/*   Updated: 2024/07/08 20:14:01 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,6 @@ void	ft_execute(struct s_node_execution *exec, char **envp)
 	int		i;
 	pid_t	pid;
 
-
 	i = 0;
 	param_count = 0;
 	command = find_the_command(envp, exec);
@@ -86,13 +85,7 @@ void	ft_execute(struct s_node_execution *exec, char **envp)
 		i++;
 	}
 	argv[param_count + 1] = NULL;
-	pid = fork();
-	if (pid == 0)
-	{
-		execve(command, argv, envp);
-		exit(EXIT_FAILURE); // Exit if execve fails
-	}
-	waitpid(pid, NULL, 0);
+	execve(command, argv, envp);
 	free(command);
 	free(argv);
 }
