fcd95396b03cb682059049d5b5965cc4ef9e9ccc
Eduardo Corona <eduardo.corona040@gmail.com>
2024-07-20 20:27:22 +0100

    Fix signal uninitizalized struct / Update Makefile for vgdb

--- minishell/signal.c
+++ minishell/signal.c
@@ -6,32 +6,32 @@
 /*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 19:57:26 by ecorona-          #+#    #+#             */
-/*   Updated: 2024/07/05 21:26:34 by ecorona-         ###   ########.fr       */
+/*   Updated: 2024/07/20 20:19:49 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <readline/readline.h>
-#include <unistd.h>
+#include "libft.h"
 
-static void	sighandler(int sig);
+static void	sighandler(int sig, siginfo_t *info, void *ucontext);
 extern int	g_sig;
 
 int	siginit(void)
 {
 	sigset_t			sa_mask;
-	struct sigaction	sa;
+	struct sigaction	act;
 
+	ft_memset(&act, 0, sizeof(struct sigaction));
 	if (sigemptyset(&sa_mask) < 0 || sigaddset(&sa_mask, SIGINT) < 0 \
-		|| sigaddset(&sa_mask, SIGQUIT) < 0 || sigaddset(&sa_mask, SIGUSR1) < 0)
+		|| sigaddset(&sa_mask, SIGQUIT) < 0)
 	{
 		printf("SIGSET INIT FAILED!\n");
 		return (0);
 	}
-	sa.sa_mask = sa_mask;
-	sa.sa_handler = sighandler;
-	if (sigaction(SIGINT, &sa, NULL) < 0 || sigaction(SIGQUIT, &sa, NULL) < 0 \
-		|| sigaction(SIGUSR1, &sa, NULL) < 0)
+	act.sa_mask = sa_mask;
+	act.sa_sigaction = sighandler;
+	act.sa_flags |= SA_SIGINFO;
+	if (sigaction(SIGINT, &act, NULL) < 0 || sigaction(SIGQUIT, &act, NULL) < 0)
 	{
 		printf("SIGACTION FAILED!\n");
 		return (0);
@@ -39,11 +39,13 @@ int	siginit(void)
 	return (0);
 }
 
-void	sighandler(int sig)
+void	sighandler(int sig, siginfo_t *info, void *ucontext)
 {
 	static char	c;
 	static int	i;
 
+	(void)info;
+	(void)ucontext;
 	g_sig = sig;
 	if (sig == SIGINT)
 	{
