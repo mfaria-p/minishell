3ec4345b4a6e7f65867175a831d7b308bc96a5ae
mfaria-p <mariana@Mariana>
2024-07-25 23:13:21 +0100

    started handling for export +=

--- minishell/exec/error_handling2.c
+++ minishell/exec/error_handling2.c
@@ -6,7 +6,7 @@
 /*   By: mfaria-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 19:15:48 by mfaria-p          #+#    #+#             */
-/*   Updated: 2024/07/18 19:16:23 by mfaria-p         ###   ########.fr       */
+/*   Updated: 2024/07/25 23:08:01 by mfaria-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,15 @@ void	error_identifier(char *var, char *value)
 		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
 	}
 }
+
+void	error_identifier_plus(char *var, char *value)
+	{
+		ft_putstr_fd("minishell: export: `", STDERR_FILENO);
+		ft_putstr_fd(var, STDERR_FILENO);
+		ft_putstr_fd("+=", STDERR_FILENO);
+		ft_putstr_fd(value, STDERR_FILENO);
+		ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+	}
 
 void	ft_free(char **envp)
 {
