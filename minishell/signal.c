6e38212b7599089404741b9f6371e001cb235f48
Eduardo Corona Oliveira Costa <ecorona-@c2r5s8.42porto.com>
2024-08-03 17:41:46 +0100

    Update child signal handling

--- minishell/signal.c
+++ minishell/signal.c
@@ -6,7 +6,7 @@
 /*   By: mfaria-p <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 19:57:26 by ecorona-          #+#    #+#             */
-/*   Updated: 2024/08/03 16:38:04 by ecorona-         ###   ########.fr       */
+/*   Updated: 2024/08/03 17:38:31 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,7 @@
 #include "libft.h"
 #include <signal.h>
 
-static void	sighandler(int sig, siginfo_t *info, void *ucontext);
-
-int	siginit(void)
-{
-	sigset_t			sa_mask;
-	struct sigaction	act;
-
-	g_sig = 0;
-	ft_memset(&act, 0, sizeof(struct sigaction));
-	if (sigemptyset(&sa_mask) < 0 || sigaddset(&sa_mask, SIGINT) < 0 \
-		|| sigaddset(&sa_mask, SIGQUIT) < 0)
-	{
-		printf("SIGSET INIT FAILED!\n");
-		return (0);
-	}
-	act.sa_mask = sa_mask;
-	act.sa_sigaction = sighandler;
-	act.sa_flags |= SA_SIGINFO;
-	if (sigaction(SIGINT, &act, NULL) < 0 || sigaction(SIGQUIT, &act, NULL) < 0)
-	{
-		printf("SIGACTION FAILED!\n");
-		return (0);
-	}
-	return (0);
-}
-
-void	sighandler(int sig, siginfo_t *info, void *ucontext)
+static void	sighandler(int sig, siginfo_t *info, void *ucontext)
 {
 	(void)info;
 	(void)ucontext;
@@ -52,6 +26,26 @@ void	sighandler(int sig, siginfo_t *info, void *ucontext)
 		rl_on_new_line();
 		rl_redisplay();
 	}
+}
+
+int	siginit(void)
+{
+	struct sigaction	act_int;
+	struct sigaction	act_quit;
+
+	g_sig = 0;
+	ft_memset(&act_int, 0, sizeof(struct sigaction));
+	ft_memset(&act_quit, 0, sizeof(struct sigaction));
+	act_int.sa_sigaction = sighandler;
+	act_int.sa_flags |= SA_SIGINFO;
+	act_quit.sa_handler = SIG_IGN;
+	if (sigaction(SIGINT, &act_int, NULL) < 0 || \
+		sigaction(SIGQUIT, &act_quit, NULL) < 0)
+	{
+		printf("SIGACTION FAILED!\n");
+		return (0);
+	}
+	return (0);
 }
 
 void	sigignore(void)
@@ -71,4 +65,27 @@ void	sigchild(void)
 	act.sa_handler = SIG_DFL;
 	sigaction(SIGQUIT, &act, NULL);
 	sigaction(SIGINT, &act, NULL);
+}
+
+void	child_signal(int wstatus)
+{
+	int		sig;
+	int		nl;
+
+	nl = 0;
+	if (WIFSIGNALED(wstatus))
+	{
+		sig = WTERMSIG(wstatus);
+		if (sig == SIGINT)
+			nl = 1;
+		else if (sig == SIGQUIT)
+		{
+			write(STDERR_FILENO, "Quit", 4);
+			nl = 1;
+		}
+		if (WCOREDUMP(wstatus))
+			write(STDERR_FILENO, " (core dumped)", 14);
+	}
+	if (nl)
+		write(STDERR_FILENO, "\n", 1);
 }
