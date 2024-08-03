20907c54a08c323c7bd36617ed875336ea2f72ee
Eduardo Corona Oliveira Costa <ecorona-@c2r5s8.42porto.com>
2024-08-03 14:46:09 +0100

    Fix norminette

--- minishell/exec/ft_exit.c
+++ minishell/exec/ft_exit.c
@@ -6,16 +6,16 @@
 /*   By: ecorona- <ecorona-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 18:07:20 by ecorona-          #+#    #+#             */
-/*   Updated: 2024/08/03 12:58:45 by ecorona-         ###   ########.fr       */
+/*   Updated: 2024/08/03 14:45:47 by ecorona-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-void  ft_exit(char **params, int *wstatus)
+void	ft_exit(char **params, int *wstatus)
 {
-	const char *err1 = "minishell: exit: too many arguments\n";
-	const char *err2 = "minishell: exit: numeric argument required\n";
+	const char	*err1 = "minishell: exit: too many arguments\n";
+	const char	*err2 = "minishell: exit: numeric argument required\n";
 
 	*wstatus = 0;
 	if (params)
